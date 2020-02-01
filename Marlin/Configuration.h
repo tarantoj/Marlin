4afb549d17d5d0d2bdec7494e12510f19716f97c
James Taranto <taranto.james@gmail.com>
2020-02-01 21:53:08 +1100

    Update e-step

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -605,7 +605,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 79.8, 79.8, 402, 101.68 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 79.8, 79.8, 402, 98 }
 
 /**
  * Default Max Feed Rate (mm/s)
