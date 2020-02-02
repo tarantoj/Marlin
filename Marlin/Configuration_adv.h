e65a55c75eb836cb2aa32a6c2fcf77359fb0d74d
James Taranto <taranto.james@gmail.com>
2020-02-02 12:46:33 +1100

    Tune JUNCTION_DEVIATION_MM

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -510,7 +510,7 @@
 //
 #define JUNCTION_DEVIATION
 #if ENABLED(JUNCTION_DEVIATION)
-  #define JUNCTION_DEVIATION_MM 0.075  // (mm) Distance from real junction edge
+  #define JUNCTION_DEVIATION_MM 0.05  // (mm) Distance from real junction edge
 #endif
 
 /**
