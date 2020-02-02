8ad214ef1780c7c9649f4aafb035ddb211a7ecaa
James Taranto <taranto.james@gmail.com>
2020-02-02 22:15:14 +1100

    Add PID_AUTOTUNE results

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -372,9 +372,9 @@
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
   // Creality Ender-3
-  #define DEFAULT_Kp 21.73
-  #define DEFAULT_Ki 1.54
-  #define DEFAULT_Kd 76.55
+  // #define DEFAULT_Kp 21.73
+  // #define DEFAULT_Ki 1.54
+  // #define DEFAULT_Kd 76.55
 
   // Ultimaker
   //#define DEFAULT_Kp 22.2
@@ -390,6 +390,11 @@
   //#define DEFAULT_Kp 63.0
   //#define DEFAULT_Ki 2.25
   //#define DEFAULT_Kd 440
+
+  // Auto tune results
+  #define DEFAULT_Kp 21.31
+  #define DEFAULT_Ki 1.61
+  #define DEFAULT_Kd 70.69
 
 #endif // PIDTEMP
 
