49ad5714615ef3028347abf7fbd8b0d48dffabe5
James Taranto <taranto.james@gmail.com>
2020-02-03 20:22:19 +1100

    Disable LIN_ADVANCE

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -835,7 +835,7 @@
  */
 #define LIN_ADVANCE
 #if ENABLED(LIN_ADVANCE)
-  #define LIN_ADVANCE_K 0.8  // Unit: mm compression per 1mm/s extruder speed
+  #define LIN_ADVANCE_K 0  // Unit: mm compression per 1mm/s extruder speed
   //#define LA_DEBUG          // If enabled, this will generate debug information output over USB.
 #endif
 
